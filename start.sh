9d64d39c9b0f327bd30f538417f712e4854adc89
Colin Wilson <colin.wilson@protonmail.ch>
2016-08-19 08:55:52 +0100

    fix error in sed command that updates worker_processes value.

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Update nginx to match worker_processes to no. of cpu's
 procs=$(cat /proc/cpuinfo |grep processor | wc -l)
-sed -i -e "s/worker_processes 1/worker_processes $procs/" /etc/nginx/nginx.conf
+sed -i -e "s/worker_processes  1/worker_processes $procs/" /etc/nginx/nginx.conf
 
 # Start supervisord and services
 /usr/local/bin/supervisord -n -c /etc/supervisord.conf
